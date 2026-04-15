9b75b322ecfe2871bf899601a63f7726b7498061
BajP1 <bajproject01@gmail.com>
2026-04-15 16:41:57 +0530

    Create Refund.tsx

---
+++ src/pages/Refund.tsx
@@ -0,0 +1,12 @@
+const Refund = () => {
+  return (
+    <div style={{ padding: "40px", color: "white" }}>
+      <h1>Refund and Cancellation Policy</h1>
+      <p>
+        Bookings once made are non-refundable. In case of issues, contact support.
+      </p>
+    </div>
+  );
+};
+
+export default Refund;
