67de8296c85f2339b37ef5994a87b55a866a3f48
BajP1 <bajproject01@gmail.com>
2026-04-15 16:41:09 +0530

    Create Terms.tsx

---
+++ src/pages/Terms.tsx
@@ -0,0 +1,13 @@
+const Terms = () => {
+  return (
+    <div style={{ padding: "40px", color: "white" }}>
+      <h1>Terms and Conditions</h1>
+      <p>
+        By using Mythical Gaming Station, you agree to follow our rules and policies.
+        All bookings are subject to availability.
+      </p>
+    </div>
+  );
+};
+
+export default Terms;
